51817c4b433a92b631ca72facd899112ecdfa8db
prosfp <110301233+prosfp@users.noreply.github.com>
2023-11-22 18:44:17 +0100

    working Tailwind - Commit Pablo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  purge: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
